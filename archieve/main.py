33f4a52df03ace7bfc0679d29411b15f11a32764
DTJ-Tran <Dat.TT228006@sis.hust.edu.vn>
2025-05-06 11:14:54 +0700

    pull the k8s Down

--- archieve/main.py
+++
@@ -1,5 +0,0 @@
-# main.py
-import get_model_2
-
-# main.py
-exec(open("get_model_2.py").read())
--- setup.py
+++
@@ -1,7 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name='vifinancenews',  # or any name you prefer
-    version='0.1',
-    packages=find_packages(),
-)
